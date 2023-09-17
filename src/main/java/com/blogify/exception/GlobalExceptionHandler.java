35584bf2b40a3851ea3002bbc736ccfeaac7fbe9
Marcin Seweryn <marcinsewerynn@gmail.com>
2023-09-17 06:34:36 +0200

    update and refactor exception handler

--- src/main/java/com/blogify/exception/GlobalExceptionHandler.java
+++ src/main/java/com/blogify/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.blogify.exception;
 
 import com.blogify.payload.ErrorResponse;
+import jakarta.mail.MessagingException;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
@@ -22,8 +23,6 @@ import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.springframework.http.HttpStatus.UNAUTHORIZED;
-
 @Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
@@ -31,127 +30,70 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(ApiException.class)
     public ResponseEntity<ErrorResponse> handleApiException(ApiException ex) {
         log.debug("Handling ApiException", ex);
-
         return handleApiExceptionInternal(ex);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleAllExceptions(Exception ex) {
         log.debug("Handling all exceptions", ex);
-
-        ErrorResponse error = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(),
-                                                ex.getMessage(), Instant.now().getEpochSecond()
-        );
-
-        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+        return buildErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
     }
 
     @ExceptionHandler(LockedException.class)
-    public ResponseEntity<ErrorResponse> handleException(LockedException exp) {
-        return ResponseEntity.status(UNAUTHORIZED).body(
-                ErrorResponse.builder().timestamp(Instant.now().getEpochSecond()).status(
-                        UNAUTHORIZED.value()).message(exp.getMessage()).build());
+    public ResponseEntity<ErrorResponse> handleLockedException(LockedException ex) {
+        return buildErrorResponse(HttpStatus.UNAUTHORIZED, ex.getMessage());
     }
 
     @ExceptionHandler(DisabledException.class)
-    public ResponseEntity<ErrorResponse> handleException(DisabledException exp) {
-        return ResponseEntity.status(UNAUTHORIZED).body(
-                ErrorResponse.builder().timestamp(Instant.now().getEpochSecond()).status(
-                        UNAUTHORIZED.value()).message(exp.getMessage()).build());
+    public ResponseEntity<ErrorResponse> handleDisabledException(DisabledException ex) {
+        return buildErrorResponse(HttpStatus.UNAUTHORIZED, ex.getMessage());
     }
-
 
     @ExceptionHandler(BadCredentialsException.class)
-    public ResponseEntity<ErrorResponse> handleException() {
-        return ResponseEntity.status(UNAUTHORIZED).body(
-                ErrorResponse.builder().timestamp(Instant.now().getEpochSecond()).status(
-                        UNAUTHORIZED.value()).message("Bad credentials").build());
+    public ResponseEntity<ErrorResponse> handleBadCredentialsException() {
+        return buildErrorResponse(HttpStatus.UNAUTHORIZED, "Bad credentials");
     }
 
+    @ExceptionHandler(MessagingException.class)
+    public ResponseEntity<ErrorResponse> handleMessagingException(MessagingException ex) {
+        return buildErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
+    }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(
-            IllegalArgumentException ex) {
-        log.debug("Handling forbidden exception", ex);
-
-        ErrorResponse error = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage(),
-                                                Instant.now().getEpochSecond()
-        );
-
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return buildErrorResponse(HttpStatus.BAD_REQUEST, ex.getMessage());
     }
 
-
     @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(
-            MethodArgumentNotValidException ex, @NonNull HttpHeaders headers,
-            @NonNull HttpStatusCode status, @NonNull WebRequest request) {
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, @NonNull HttpHeaders headers, @NonNull HttpStatusCode status, @NonNull WebRequest request) {
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getAllErrors().forEach((error) -> {
             String fieldName = ((FieldError) error).getField();
             String message = error.getDefaultMessage();
             errors.put(fieldName, message);
         });
-
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
-
     @ExceptionHandler(AccessDeniedException.class)
     public ResponseEntity<ErrorResponse> handleForbiddenException(AccessDeniedException ex) {
-        log.debug("Handling forbidden exception", ex);
-
-        ErrorResponse error = new ErrorResponse(HttpStatus.FORBIDDEN.value(), ex.getMessage(),
-                                                Instant.now().getEpochSecond()
-        );
-
-        return new ResponseEntity<>(error, HttpStatus.FORBIDDEN);
+        return buildErrorResponse(HttpStatus.FORBIDDEN, ex.getMessage());
     }
-
 
     private ResponseEntity<ErrorResponse> handleApiExceptionInternal(ApiException ex) {
         log.debug("Assigning correct status to ResponseEntity", ex);
+        HttpStatus status = ex.getStatus() != null ? ex.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
+        return buildErrorResponse(status, ex.getMessage());
+    }
 
-        ErrorResponse errorResponse;
-        HttpStatus status = ex.getStatus();
-
-        switch (status) {
-            case NOT_FOUND:
-                errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage(),
-                                                  Instant.now().getEpochSecond()
-                );
-                break;
-
-            case BAD_REQUEST:
-                errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage(),
-                                                  Instant.now().getEpochSecond()
-                );
-                break;
-
-            case METHOD_NOT_ALLOWED:
-                errorResponse = new ErrorResponse(HttpStatus.METHOD_NOT_ALLOWED.value(),
-                                                  ex.getMessage(), Instant.now().getEpochSecond()
-                );
-                break;
-
-            case CONFLICT:
-                errorResponse = new ErrorResponse(HttpStatus.CONFLICT.value(),
-                                                  ex.getMessage(), Instant.now().getEpochSecond()
-                );
-                break;
-
-            default:
-                errorResponse = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(),
-                                                  ex.getMessage(), Instant.now().getEpochSecond()
-                );
-                status = HttpStatus.INTERNAL_SERVER_ERROR;
-                break;
-        }
-
-        log.debug("Finish handling ApiException", ex);
+    private ResponseEntity<ErrorResponse> buildErrorResponse(HttpStatus status, String message) {
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .timestamp(Instant.now().getEpochSecond())
+                .status(status.value())
+                .message(message)
+                .build();
         log.debug("Status {}", status);
         log.debug("ErrorResponse {}", errorResponse);
-
         return new ResponseEntity<>(errorResponse, status);
     }
 }
